fcbe51b1d5e81ea3c5b4c784ef0d2132765b6bd3
Julio Napurí <julionc@gmail.com>
2025-09-06 16:42:58 -0500

    chore: add toogle file explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,7 +19,6 @@ vim.api.nvim_create_user_command("WQ", "wq", {})
 vim.api.nvim_create_user_command("Wq", "wq", {})
 
 -- Plugins
-map("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle file explorer" })
 map("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" })
 map("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live grep" })
 map("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find buffers" })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -21,8 +21,17 @@ return {
         filesystem = {
           follow_current_file = { enabled = true },
           use_libuv_file_watcher = true,
+          filtered_items = {
+            visible = true,         -- Show hidden items by default
+            hide_dotfiles = false,  -- Show dotfiles (.config, .env, etc.)
+            hide_gitignored = false -- Show files ignored by .gitignore
+          },
         },
       })
+
+      -- Keymap: toggle Neo-tree
+      vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", { desc = "Toggle file explorer" })
+
     end,
   }
 }
